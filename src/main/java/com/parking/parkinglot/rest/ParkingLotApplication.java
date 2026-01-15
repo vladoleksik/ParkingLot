7385796fc5da48a93ee30202e207a0a38fadfa74
Vlad-Andrei Oleksik <vladandrei.oleksik@gmail.com>
2026-01-15 23:01:32 +0200

    Add RESTful controller for cars

---
+++ src/main/java/com/parking/parkinglot/rest/ParkingLotApplication.java
@@ -0,0 +1,8 @@
+package com.parking.parkinglot.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/resources")
+public class ParkingLotApplication extends Application {
+}
